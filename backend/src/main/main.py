e3b346d66e8557603411fe8b5816b6b733942b58
Lukas Lay <lay.lukas@gmx.de>
2025-04-21 22:02:19 +0200

    feat: Added multiple endpoints for route calculation and station management

--- backend/src/main/main.py
+++ backend/src/main/main.py
@@ -8,13 +8,3 @@ app.include_router(train_controller.router, prefix="/api", tags=["Route"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-
-
-#from fastapi import FastAPI
-# from controller import train_controller
-
-# app = FastAPI()
-
-# app.include_router(train_controller.router, prefix="/api", tags=["Route"])
